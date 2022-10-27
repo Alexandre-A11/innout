52341b7df47f61115df1e5ff4da95ebfda2f2777
Alexandre-A11 <alee.wxa@gmail.com>
2022-10-27 11:08:24 -0300

    fix: exception mysqli, feat: Ober Usuários Banco

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
-<h1>Hello World!</h1>
+<?php
+
+require_once(dirname(__FILE__, 2) . "/src/config/config.php");
+require_once(dirname(__FILE__, 2) . "/src/models/User.php");
+
+$user = new User(["name" => "Alexandre", "email" => "ale.cod3r.com"]);
+
+print_r(User::get(["id" => 1], "id, name, email"));
+echo "<br>";
+print_r(User::get(["name" => "Chaves"], "id, name, email"));
+echo "<br>";
+
+foreach (User::get([], "name") as $user){
+    echo $user->name;
+    echo "<br>";
+}
--- src/models/Model.php
+++ src/models/Model.php
@@ -25,11 +25,30 @@ class Model {
         $this->values[$key] = $value;
     }
 
-    public static function getSelect($filters = [], $columns = "*"){
-        $sql = "SELECT {$columns} FROM " 
+    public static function get($filters = [], $columns = "*"){
+        $objects = [];
+        $result = static::getResultSetFromSelect($filters, $columns);
+
+        if ($result) {
+            $class = get_called_class();
+            while ($row = $result->fetch_assoc()){
+                array_push($objects, new $class($row));
+            }
+        }
+
+        return $objects;
+    }
+
+    public static function getResultSetFromSelect($filters = [], $columns = "*"){
+        $sql = "SELECT ${columns} FROM " 
         . static::$tableName
         . static::getFilters($filters);
-        return $sql;
+
+        $result = Database::getResultFromQuery($sql);
+
+        if ($result->num_rows === 0) return null;
+
+        return $result;
     }
 
     private static function getFilters($filters) {
@@ -48,6 +67,8 @@ class Model {
             return "null";
         } elseif(gettype($value) == "string") {
             return "'{$value}'";
+        } else {
+            return $value;
         }
     }
 }
