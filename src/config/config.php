59919eb4eddb15d5602ce1805b68a1cd8f625aa5
Alexandre-A11 <alee.wxa@gmail.com>
2022-11-01 09:40:25 -0300

    feat: Validar login

--- src/config/config.php
+++ src/config/config.php
@@ -5,5 +5,8 @@ setlocale(LC_TIME, "pt_BR", "pt_BR.utf-8", "portuguese");
 
 // Folders
 define("MODEL_PATH", realpath(dirname(__FILE__) . "/../models"));
+define("VIEW_PATH", realpath(dirname(__FILE__) . "/../views"));
 
+// Files
 require_once(realpath(dirname(__FILE__) . "/database.php"));
+require_once(realpath(MODEL_PATH . "/Model.php"));
