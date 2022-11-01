2c2ae12f5a3f255e6685c992efeee38413b52e29
Alexandre-A11 <alee.wxa@gmail.com>
2022-11-01 09:15:14 -0300

    feat: Tela login

--- public/index.php
+++ public/index.php
@@ -1,16 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . "/src/config/config.php");
-require_once(dirname(__FILE__, 2) . "/src/models/User.php");
-
-$user = new User(["name" => "Alexandre", "email" => "ale.cod3r.com"]);
-
-print_r(User::get(["id" => 1], "id, name, email"));
-echo "<br>";
-print_r(User::get(["name" => "Chaves"], "id, name, email"));
-echo "<br>";
-
-foreach (User::get([], "name") as $user){
-    echo $user->name;
-    echo "<br>";
-}
+require_once(dirname(__FILE__, 2) . "/src/views/login.php");
