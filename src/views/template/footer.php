d3250434f1e6073cb892f10d5aba798cf85d9785
Alexandre-A11 <alee.wxa@gmail.com>
2022-11-04 13:53:41 -0300

    feat: header & footer

---
+++ src/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span>por</span>
+    <span><span class="text-danger">A</span>lexandre</span>
+</footer>
+</body>
+</html>
---
+++ src/views/template/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/template.css">
+    <title>In N' Out</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo">
+            <i class="icofont-travelling mr-2"></i>
+            <span class="font-weight-light">In</span>
+            <span class="font-weight-bold mx-1">N'</span>
+            <span class="font-weight-light">Out</span>
+            <i class="icofont-runner-alt-1 ml-2"></i>
+        </div>
+        <div class="menu-toggle mx-3">
+            <i class="iconfont-navigation-menu"></i>
+        </div>
+        <div class="spacer"></div>
+    </header>
