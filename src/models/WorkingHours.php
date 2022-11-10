aee615a7ebf9bba60bea38c14cda8e7cbe14d328
Alexandre-A11 <alee.wxa@gmail.com>
2022-11-10 10:40:55 -0300

    feat: Calcula Intervalo Almoço

--- src/models/WorkingHours.php
+++ src/models/WorkingHours.php
@@ -44,4 +44,39 @@ class WorkingHours extends Model {
         $this->$timeColumn = $time;
         $this->id ? $this->update() : $this->insert();
     }
+
+    function getWorkedInterval() {
+        [$t1, $t2, $t3, $t4] = $this->getTimes();
+
+        $part1 = new DateInterval("PT0S");
+        $part2 = new DateInterval("PT0S");
+
+        if ($t1) $part1 = $t1->diff(new DateTime());
+        if ($t2) $part1 = $t1->diff($t2);
+        if ($t3) $part2 = $t3->diff(new DateTime());
+        if ($t4) $part2 = $t3->diff($t4);
+        
+        return sumIntervals($part1, $part2);
+    }
+    
+    function getLunchInterval() {
+        [, $t2, $t3, ] =  $this->getTimes();
+        $breakInterval = new DateInterval("PT0S");
+        
+        if ($t2) $breakInterval = $t2->diff(new DateTime());
+        if ($t3) $breakInterval = $t3->diff($t2);
+
+        return $breakInterval;
+    }
+
+   private function getTimes() {
+    $times = [];
+
+    $this->time1 ? array_push($times, getDateFromString($this->time1)) : array_push($times, null);
+    $this->time2 ? array_push($times, getDateFromString($this->time2)) : array_push($times, null);
+    $this->time3 ? array_push($times, getDateFromString($this->time3)) : array_push($times, null);
+    $this->time4 ? array_push($times, getDateFromString($this->time4)) : array_push($times, null);
+
+    return $times;
+   } 
 }
