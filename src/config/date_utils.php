aba007639bda7b8782d7667fce9c02b752d069bb
Alexandre-A11 <alee.wxa@gmail.com>
2022-11-10 09:56:31 -0300

    feat: Intervalos de Tempos

--- src/config/date_utils.php
+++ src/config/date_utils.php
@@ -20,3 +20,26 @@ function getNextDay($date) {
     $inputDate->modify("+1 day");
     return $inputDate;
 }
+
+function sumIntervals($interval1, $interval2) {
+    $date = new DateTime("00:00:00");
+    $date->add($interval1);
+    $date->add($interval2);
+    return (new DateTime("00:00:00"))->diff($date);
+}
+
+function subtractIntervals($interval1, $interval2) {
+    $date = new DateTime("00:00:00");
+    $date->add($interval1);
+    $date->sub($interval2);
+    return (new DateTime("00:00:00"))->diff($date);
+
+}
+
+function getDateFromInterval($interval) {
+    return new DateTimeImmutable($interval->format("%H:%i:%s"));
+}
+
+function getDateFromString($str) {
+    return DateTimeImmutable::createFromFormat("H:i:s", $str);
+}
